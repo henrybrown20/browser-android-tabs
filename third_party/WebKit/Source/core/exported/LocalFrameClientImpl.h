44cd04395db352ef7bfefaf97815a727b5cbbb5b
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-07-19 16:06:31 -0400

    Customize UA for duckduckgo

--- third_party/WebKit/Source/core/exported/LocalFrameClientImpl.h
+++ third_party/WebKit/Source/core/exported/LocalFrameClientImpl.h
@@ -150,7 +150,7 @@ class LocalFrameClientImpl final : public LocalFrameClient {
                                        const ResourceRequest&,
                                        const SubstituteData&,
                                        ClientRedirectPolicy) override;
-  WTF::String UserAgent() override;
+  WTF::String UserAgent(const std::string& strUrl) override;
   WTF::String DoNotTrackValue() override;
   void TransitionToCommittedForNewPage() override;
   LocalFrame* CreateFrame(const WTF::AtomicString& name,
@@ -256,6 +256,7 @@ class LocalFrameClientImpl final : public LocalFrameClient {
   Member<WebLocalFrameImpl> web_frame_;
 
   String user_agent_;
+  std::string previous_url_;
 };
 
 DEFINE_TYPE_CASTS(LocalFrameClientImpl,
